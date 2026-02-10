4270ea940de7313d2dd2a40ccd3fcc1f1550b890
himanshushriva <shriva.himanshu@gmail.com>
2026-02-10 11:53:07 +0530

    setup view binding & change theme for action bar

--- app/src/main/java/com/himan/bookexpo/ui/main/MainActivity.kt
+++ app/src/main/java/com/himan/bookexpo/ui/main/MainActivity.kt
@@ -2,6 +2,7 @@ package com.himan.bookexpo.ui.main
 
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,17 +11,20 @@ import androidx.lifecycle.lifecycleScope
 import com.himan.bookexpo.R
 import com.himan.bookexpo.data.remote.BookApi
 import com.himan.bookexpo.data.remote.RetrofitHelper
+import com.himan.bookexpo.databinding.ActivityMainBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     private val TAG: String = "MainActivity"
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
