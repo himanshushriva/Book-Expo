200066083ababd1dfe0c3135392b03ca6f75afd7
himanshushriva <shriva.himanshu@gmail.com>
2026-02-11 17:30:35 +0530

    setup action bar drawer toggle on the home button & add click functionality to it

--- app/src/main/java/com/himan/bookexpo/ui/main/MainActivity.kt
+++ app/src/main/java/com/himan/bookexpo/ui/main/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.himan.bookexpo.ui.main
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.himan.bookexpo.R
@@ -12,6 +15,7 @@ class MainActivity : AppCompatActivity() {
 
     private val TAG = "MainActivity"
     private lateinit var binding: ActivityMainBinding
+    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +26,7 @@ class MainActivity : AppCompatActivity() {
 
         setupInsets()
         setUpToolbar()
+        setupDrawer()
     }
 
     private fun setupInsets() {
@@ -33,11 +38,31 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setUpToolbar() {
-        setSupportActionBar(binding.toolbar)
+        //setSupportActionBar(binding.toolbar)
         supportActionBar?.apply {
             title = "Toolbar"
             setHomeButtonEnabled(true)
-            setDisplayHomeAsUpEnabled(true)
+            //setDisplayHomeAsUpEnabled(true)
         }
+    }
+
+    private fun setupDrawer() {
+        actionBarDrawerToggle = ActionBarDrawerToggle(
+            this,
+            binding.drawerLayout,
+            binding.toolbar,
+            R.string.drawer_open,
+            R.string.drawer_close
+        )
+        binding.drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+    }
+
+    // Add functionality/clicks on the ActionBar (which has Hamburger icon for the navigation drawer)
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            binding.drawerLayout.openDrawer(GravityCompat.START)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
