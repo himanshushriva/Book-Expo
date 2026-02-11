5d06b23f9bcd7d9413131a2910aa815811c6f2d0
himanshushriva <shriva.himanshu@gmail.com>
2026-02-11 16:55:30 +0530

    refactor: clean up and organize MainActivity structure

--- app/src/main/java/com/himan/bookexpo/ui/main/MainActivity.kt
+++ app/src/main/java/com/himan/bookexpo/ui/main/MainActivity.kt
@@ -1,43 +1,43 @@
 package com.himan.bookexpo.ui.main
 
 import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.lifecycle.lifecycleScope
 import com.himan.bookexpo.R
-import com.himan.bookexpo.data.remote.BookApi
-import com.himan.bookexpo.data.remote.RetrofitHelper
 import com.himan.bookexpo.databinding.ActivityMainBinding
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
-    private val TAG: String = "MainActivity"
-    lateinit var binding: ActivityMainBinding
+
+    private val TAG = "MainActivity"
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setupInsets()
+        setUpToolbar()
+    }
+
+    private fun setupInsets() {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.drawerLayout)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        setUpToolbar()
     }
 
     fun setUpToolbar() {
         setSupportActionBar(binding.toolbar)
-        supportActionBar?.title = "Toolbar"
-        supportActionBar?.setHomeButtonEnabled(true)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.apply {
+            title = "Toolbar"
+            setHomeButtonEnabled(true)
+            setDisplayHomeAsUpEnabled(true)
+        }
     }
 }
