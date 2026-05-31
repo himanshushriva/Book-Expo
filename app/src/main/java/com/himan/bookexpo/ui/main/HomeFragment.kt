273411b7ae844fe83aa5faa324fb44d9119ce7f7
himanshushriva <shriva.himanshu@gmail.com>
2026-05-31 14:34:21 +0530

    Add HomeFragment

---
+++ app/src/main/java/com/himan/bookexpo/ui/main/HomeFragment.kt
@@ -0,0 +1,26 @@
+package com.himan.bookexpo.ui.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.himan.bookexpo.R
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+    }
+}
