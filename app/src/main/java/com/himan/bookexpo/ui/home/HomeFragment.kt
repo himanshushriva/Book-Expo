112486b46689effae0b6b6f0c19a8ee20bc4ea85
himanshushriva <shriva.himanshu@gmail.com>
2026-06-02 13:28:08 +0530

    Set up ViewBinding in HomeFragment

--- app/src/main/java/com/himan/bookexpo/ui/home/HomeFragment.kt
+++ app/src/main/java/com/himan/bookexpo/ui/home/HomeFragment.kt
@@ -1,25 +1,40 @@
 package com.himan.bookexpo.ui.home
 
 import android.os.Bundle
-import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.himan.bookexpo.R
+import com.himan.bookexpo.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment(R.layout.fragment_home) {
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding: FragmentHomeBinding
+        get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
     }
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        //binding.textView.text = binding.toString()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-
+        _binding = null
     }
 }
