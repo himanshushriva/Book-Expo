70ceba76e045cf96c628f85ba131db1df81eb2f6
himanshushriva <shriva.himanshu@gmail.com>
2026-06-03 23:36:03 +0530

    refactor: expose BookApi from ApiClient

--- app/src/main/java/com/himan/bookexpo/data/remote/ApiClient.kt
+++ app/src/main/java/com/himan/bookexpo/data/remote/ApiClient.kt
@@ -2,15 +2,20 @@ package com.himan.bookexpo.data.remote
 
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 
 object ApiClient {
 
     private const val BASE_URL = "https://www.dbooks.org/api/"
 
-    val retrofit: Retrofit by lazy {
+    private val retrofit: Retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
+    }
+
+    val bookApi: BookApi by lazy {
+        retrofit.create()
     }
 }
